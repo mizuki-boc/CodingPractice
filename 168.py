685804035b076f64cb5ce056dfb8d10aec0e3122
mizuki-boc <mizuki0842boc@icloud.com>
2020-05-19 10:42:01 +0900

    try ABC168

---
+++ 168.py
@@ -0,0 +1,24 @@
+#A
+# n = int(input())
+# honnum = [2, 4, 5, 7, 9]
+# ponnum = [0, 1, 6, 8]
+# bonnum = [3]
+# if n % 10 in honnum:
+#     print("hon")
+# elif n % 10 in ponnum:
+#     print("pon")
+# elif n % 10 in bonnum:
+#     print("bon")
+
+#B
+# k = int(input())
+# s = input()
+# if len(s) > k:
+#     print(s[:k] + "...")
+# else:
+#     print(s)
+
+#C
+# import math
+# a, b, h, m = [int(_) for _ in input().split()]
+# print(math.sqrt(b ** 2 - 2 * a * b * math.cos(math.radians(abs(30*h+m/2 - 6*m))) + a ** 2))
